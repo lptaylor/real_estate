2b42db6e6b1cc480c335fc1825e9e5982c44da02
Lance Taylor <lance.taylor98@hotmail.com>
2018-10-17 10:14:26 -0600

    adds iter. 3 selection method good

--- lib/house.rb
+++ lib/house.rb
@@ -12,9 +12,9 @@ class House
     @rooms << room
   end
 
-  def rooms_from_catagory(catagory)
+  def rooms_from_catagory(type)
     @rooms.select do |room|
-      room.catagory
+      room.catagory == type
     end
   end
 
